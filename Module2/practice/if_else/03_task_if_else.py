29a210e6f8ad77dd422f2a6517b26dafbec29f77
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-09-19 21:29:05 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -3,3 +3,12 @@
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
 # TODO: your code here
+x = int (input("Enter edge 1 length "))
+y = int (input("Enter edge 2 length "))
+z = int (input("Enter edge 3 length "))
+if x == y or y == z:
+    print ("Yes")
+elif x == z:
+    print ("Yes")
+else:
+    print ("No")
