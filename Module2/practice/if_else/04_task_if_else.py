73ec815a950e0d0597aea86cd57f905231207c5b
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-09-19 21:50:11 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,18 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+x = int (input("Enter number "))
+
+if x % 3 == 0 and x % 5 != 0:
+    print ("Foo")
+if x % 5 == 0 and x % 3 != 0:
+    print ("Bar")
+if x % 3 == 0 and x % 5 == 0:
+    print ("Foobar")
+'''
+# в случае, если Foobar == FooBar
+if x % 3 == 0:
+    print ("Foo", end = "")
+if x % 5 == 0:
+    print ("Bar")
+'''
