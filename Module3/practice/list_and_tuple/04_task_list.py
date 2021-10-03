58274bb4f1e922662b0541bf078c5c832f7559e1
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-10-03 12:10:38 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+import random
+n = 10
+list = []
+i = 0
+
+while i < n:
+    list.append(random.randint(-10, 10))
+    i += 1
+
+s = 0
+for el in list:
+    if el > 0:
+        s += el
+print(list) # проверка
+print(s)
