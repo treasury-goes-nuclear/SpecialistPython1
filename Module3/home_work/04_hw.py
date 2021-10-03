4ab04afa04eb33598ddcf02e6b953719a01261f2
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-10-03 07:51:04 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,12 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+numbers = [2, -5, 8, 9, -25, 25, 4] # при заполении случайными числами подходящие элементы почти не появлялись
+good_numbers = []
+
+i = 0
+while i < len(numbers):
+    if numbers[i] > 0 and (numbers[i] ** 0.5 - int(numbers[i] ** 0.5) == 0):
+        good_numbers.append(int(numbers[i]**0.5))
+    i += 1
+print(good_numbers)
