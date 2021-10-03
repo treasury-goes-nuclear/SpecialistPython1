866a3d196b41af1631d6a6edffc3f77c7574f4d9
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-10-03 12:08:33 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,15 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+n = 10
+list = []
+i = 0
+s = 0
+while i < n:
+    list.append(random.randint(-10, 10))
+    i += 1
+for el in list:
+    s += el
+print(list) # проверка
+print(s)
