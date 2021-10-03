b8c966dfc8aed44a5c43f017cb7d9b2f73a5705f
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-10-03 07:22:29 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -6,3 +6,8 @@ import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+n = 11
+i = 0
+while i < n:
+    numbers.append(random.randint(-100, 100))
+    i += 1
