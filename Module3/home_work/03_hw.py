cc518d4fb1c6ae0f416055e31ec134907226fe1b
treasury-goes-nuclear <91006255+treasury-goes-nuclear@users.noreply.github.com>
2021-10-03 07:28:47 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+numbers = []
+n = 11
+i = 0 # counter
+while i < n:
+    numbers.append(random.randint(-100, 100))
+    i += 1
+print(numbers) # to check result
+i = 0
+s = 0 # sum
+while i < n:
+    if numbers[i] > 0 and numbers[i] % 2 == 0:
+        s = s + numbers[i]
+    i += 1
+print(s)
